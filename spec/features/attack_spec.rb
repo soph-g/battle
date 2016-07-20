ff143f0e43c04788062ca04d599be0c6b1db9110
Sophie Gill <sophie-gill@hotmail.com>
2016-07-20 14:02:20 +0100

    Chapter 24

--- spec/features/attack_spec.rb
+++ spec/features/attack_spec.rb
@@ -1,7 +1,17 @@
 feature 'Attack' do
+
   scenario 'Player 1 attacks player 2 and gets confirmation' do
     sign_in_and_play
     click_button 'Attack'
     expect(page).to(have_content("Sophie attacked Nik"))
   end
+
+  scenario 'Player 1 attacked player 2, reducing their HP by 10' do
+    sign_in_and_play
+    click_button 'Attack'
+    click_button 'OK'
+    expect(page).not_to(have_content("Nik: 60HP"))
+    expect(page).to(have_content("Nik: 50HP"))
+  end
+
 end
--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -2,10 +2,32 @@ require './lib/player'
 
 describe Player do
 
-  subject(:player) {described_class.new('Dave')}
+  subject(:sophie) { described_class.new('Sophie') }
+  subject(:nik) { described_class.new('Nik') }
 
-  it 'returns a player name' do
-  expect(player.name).to eq 'Dave'
+  describe '#name' do
+    it 'returns a player name' do
+      expect(sophie.name).to eq 'Sophie'
+    end
+  end
+
+  describe '#hit_points' do
+    it 'returns the hit points' do
+      expect(sophie.hit_points).to(eq(described_class::DEFAULT_HIT_POINTS))
+    end
+  end
+
+  describe '#attack' do
+    it 'damages the player' do
+      expect(nik).to(receive(:receive_damage))
+      sophie.attack(nik)
+    end
+  end
+
+  describe '#receive_damage' do
+    it 'reduces the players hit points' do
+      expect { nik.receive_damage }.to(change { nik.hit_points }.by(-10) )
+    end
   end
 
 end
